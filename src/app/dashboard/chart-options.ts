e7c554f82417b886638f8c95107d2b6e9d548499
Janic Scherer <janic.scherer@students.bfh.ch>
2026-03-20 00:09:04 +0100

    fix chart tooltip

--- src/app/dashboard/chart-options.ts
+++ src/app/dashboard/chart-options.ts
@@ -3,7 +3,7 @@ import { ChartOptions } from 'chart.js'; // FIXME is chart.js separately needed?
 export function buildBaseBarOptions(): ChartOptions<'bar'> {
   const style = getComputedStyle(document.documentElement);
   const bg = style.getPropertyValue('--p-content-background').trim(); // TODO maybe better option
-  const border = style.getPropertyValue('--color-gray-600').trim();
+  const gray = style.getPropertyValue('--color-gray-600').trim();
 
   return {
     maintainAspectRatio: false,
@@ -13,9 +13,9 @@ export function buildBaseBarOptions(): ChartOptions<'bar'> {
       },
       tooltip: {
         backgroundColor: bg,
-        titleColor: '#f8fafc', // TODO maybe use tailwind palette
-        bodyColor: '#cbd5e1',
-        borderColor: border,
+        titleColor: gray,
+        bodyColor: gray,
+        borderColor: gray,
         borderWidth: 1,
         cornerRadius: 10,
         padding: 12,
