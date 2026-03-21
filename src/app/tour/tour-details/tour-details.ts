e712224c46cffd14fdcd7de76de459ba9daa6efb
Janic Scherer <janic.scherer@students.bfh.ch>
2026-03-22 00:35:07 +0100

    add overfull bins card and use onpush cd strat

--- src/app/tour/tour-details/tour-details.ts
+++ src/app/tour/tour-details/tour-details.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { TourDTO } from '../tour.model';
 import { Card } from 'primeng/card';
 import { ChDateTimePipe } from '../../shared/pipes/ch-date-time.pipe';
@@ -8,11 +8,16 @@ import { ChDateTimePipe } from '../../shared/pipes/ch-date-time.pipe';
   imports: [Card, ChDateTimePipe],
   templateUrl: './tour-details.html',
   styleUrl: './tour-details.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TourDetails {
   tour = input.required<TourDTO>();
 
   emptyingsCount = computed(
     () => this.tour().binVisits.filter((visit) => visit.visitAction === 'EMPTIED').length,
+  );
+
+  overfullBins = computed(
+    () => this.tour().binVisits.filter((visit) => visit.fillLevel === 'OVERFULL').length,
   );
 }
