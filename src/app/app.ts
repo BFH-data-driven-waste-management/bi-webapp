7783dd46d26b4eb9b68961b664b6bc791afb913a
Janic Scherer <janic.scherer@students.bfh.ch>
2026-03-21 21:22:17 +0100

    restructure menu and main content margin

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { Toolbar } from 'primeng/toolbar';
 import { FormsModule } from '@angular/forms';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { filter, map, startWith } from 'rxjs';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu, Toolbar, FormsModule],
+  imports: [RouterOutlet, Menu, Toolbar, FormsModule, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -34,15 +35,33 @@ export class App {
     },
   ];
 
-  readonly noMarginRoutes = ['bin-map', 'tours'];
+  readonly linkItems: MenuItem[] = [
+    {
+      label: 'WebGIS Biel',
+      icon: 'pi pi-external-link',
+      url: 'https://biel-bienne.mapplus.ch/',
+      target: '_blank',
+    },
+    {
+      label: 'Strasseninspektorat',
+      icon: 'pi pi-external-link',
+      url: 'https://www.biel-bienne.ch/de/strasseninspektorat.html/850',
+      target: '_blank',
+    },
+  ];
 
-  readonly isNoMarginRoute = toSignal(
-    // TODO maybe there is a better solution
+  readonly routePaddingClass = toSignal(
     this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd),
-      map(() => this.noMarginRoutes.some((route) => this.router.url.startsWith(`/${route}`))),
-      startWith(this.noMarginRoutes.some((route) => this.router.url.startsWith(`/${route}`))),
+      map(() => this.getRoutePaddingClass(this.router.url)),
+      startWith(this.getRoutePaddingClass(this.router.url)),
     ),
-    { initialValue: this.noMarginRoutes.some((route) => this.router.url.startsWith(`/${route}`)) },
+    { initialValue: this.getRoutePaddingClass(this.router.url) },
   );
+
+  private getRoutePaddingClass(url: string): string {
+    if (url.startsWith('/tours')) return 'pr-4';
+    if (url.startsWith('/bin-map')) return '';
+    return 'p-6';
+  }
 }
