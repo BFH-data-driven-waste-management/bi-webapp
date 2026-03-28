5e39c76d10c39c26f01b44ea33966c5c9a78519f
Janic Scherer <janic.scherer@students.bfh.ch>
2026-03-28 22:12:26 +0100

    increase convenience for icon value card comp

--- src/app/shared/components/icon-value-card/icon-value-card.ts
+++ src/app/shared/components/icon-value-card/icon-value-card.ts
@@ -9,8 +9,8 @@ import { Card } from 'primeng/card';
 })
 export class IconValueCard {
   readonly header = input.required<string>();
-  readonly iconClass = input<string>();
+  readonly icon = input<string>();
   readonly value = input.required<string | number>();
   readonly secondaryValue = input<string | number>();
-  readonly secondaryIconClass = input<string>();
+  readonly secondaryIcon = input<string>();
 }
