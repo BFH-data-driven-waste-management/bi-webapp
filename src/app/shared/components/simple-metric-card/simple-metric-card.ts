180d7de0c94ae22e086b9d2975a213679b12b7df
Janic Scherer <janic.scherer@students.bfh.ch>
2026-04-03 00:51:03 +0200

    add render logic (trend, skeletons)

--- src/app/shared/components/simple-metric-card/simple-metric-card.ts
+++ src/app/shared/components/simple-metric-card/simple-metric-card.ts
@@ -1,14 +1,16 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Card } from 'primeng/card';
+import { Skeleton } from 'primeng/skeleton';
 
 @Component({
   selector: 'app-simple-metric-card',
-  imports: [Card],
+  imports: [Card, Skeleton],
   templateUrl: './simple-metric-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SimpleMetricCard {
   readonly header = input.required<string>();
   readonly icon = input.required<string>();
-  readonly value = input.required<string | number>();
+  readonly value = input<string | number>(); // TODO required?
+  readonly loading = input(false);
 }
--- src/app/shared/components/trend-metric-card/trend-metric-card.ts
+++ src/app/shared/components/trend-metric-card/trend-metric-card.ts
@@ -1,15 +1,40 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { Card } from 'primeng/card';
+import { Skeleton } from 'primeng/skeleton';
 
 @Component({
   selector: 'app-trend-metric-card',
-  imports: [Card],
+  imports: [Card, Skeleton],
   templateUrl: './trend-metric-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TrendMetricCard {
   readonly header = input.required<string>();
-  readonly value = input.required<string | number>();
-  readonly secondaryValue = input.required<string | number>();
-  readonly secondaryIcon = input.required<string>();
+  readonly value = input<string | number>();
+  readonly trendValue = input<number>();
+  readonly loading = input(false);
+
+  readonly trendIcon = computed(() => {
+    const trendValue = this.trendValue();
+    if (trendValue == null || trendValue === 0) {
+      return 'minus';
+    }
+
+    return trendValue > 0 ? 'arrow-up-right' : 'arrow-down-right';
+  });
+
+  readonly formattedTrendValue = computed(() => {
+    const trendValue = this.trendValue();
+    if (trendValue == null) {
+      return '';
+    }
+
+    if (trendValue === 0) {
+      return '0%';
+    }
+
+    const sign = trendValue > 0 ? '+' : '';
+
+    return `${sign}${trendValue}%`;
+  });
 }
