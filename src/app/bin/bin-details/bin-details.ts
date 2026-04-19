5046073054f56a9d9cd1267029d7270d639090a5
Janic Scherer <janic.scherer@students.bfh.ch>
2026-04-20 01:16:15 +0200

    add todo

--- src/app/bin/bin-details/bin-details.ts
+++ src/app/bin/bin-details/bin-details.ts
@@ -272,7 +272,7 @@ export class BinDetails implements OnInit {
             return EMPTY;
           }
 
-          this.loading.set(true);
+          this.loading.set(true); // TODO might not be needed
           this.binVisitFirst.set(0);
           this.loadBinVisits(binId, 0, this.binVisitRows);
           return this.binService.getBinDetailsById(binId);
